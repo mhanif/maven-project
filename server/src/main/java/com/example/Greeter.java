de9febdae79a592efa24999a5e763adbc9ef11ac
Mohammad Hanif <mhanif@brocade.com>
2017-08-28 23:38:58 -0700

    More style fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,10 @@ public class Greeter {
   }
 
   /**
-  * @param foo
-  * @return something
+  * @param someone the name of a person
+  * @return greeting string
   */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,4 @@
-/*
-* This is an example package file
-*/
+/**
+ * This is an example package file
+ */
 package com.example;
